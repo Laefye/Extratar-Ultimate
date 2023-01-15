ad63a98aed83ed68d0f7694ef6ca3ff685b72efa
Laefye <rtpurtooe22@mail.ru>
2023-01-15 22:02:49 +0300

    Бог любит троицу, надеюсь это наилучшая анимация

--- src/main/java/com/yellowfire/extratarultimate/entities/FennecEntityModel.java
+++ src/main/java/com/yellowfire/extratarultimate/entities/FennecEntityModel.java
@@ -4,9 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.yellowfire.extratarultimate.ExtratarUltimate;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.entity.model.AnimalModel;
-import net.minecraft.client.render.entity.model.EntityModelPartNames;
-import net.minecraft.client.render.entity.model.FoxEntityModel;
+import net.minecraft.client.render.entity.model.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.passive.FoxEntity;
 import net.minecraft.util.math.MathHelper;
@@ -76,28 +74,6 @@ public class FennecEntityModel extends AnimalModel<FennecEntity> {
         this.rightFrontLeg.pitch = MathHelper.cos(limbAngle * 0.6662F + 3.1415927F) * 1F * limbDistance;
         this.leftFrontLeg.pitch = MathHelper.cos(limbAngle * 0.6662F) * limbDistance;
         this.tail.yaw = MathHelper.cos(limbAngle * 0.6662F) * limbDistance;
-
-        var k = MathHelper.clamp(1f - entity.getHealth() / entity.getMaxHealth(), 0f, 1f);
-        // Уши (версия прижимание)
-//        this.rightEar.pitch = -0.2618F * k;
-//        this.rightEar.yaw = 1.2654F * k;
-//        this.rightEar.roll = -0.2182F * k;
-//        this.rightEar.pivotX = -1.8f - k;
-//        this.rightEar.pivotZ = -2.5F + k * 1.25f;
-//
-//        this.leftEar.pitch = -0.2618F * k;
-//        this.leftEar.yaw = -1.2654F * k;
-//        this.leftEar.roll = 0.2182F * k;
-//        this.leftEar.pivotX = 1.8f + k;
-//        this.leftEar.pivotZ = -2.5F + k * 1.25f;
-        // Уши (версия опускание)
-//        this.leftEar.pitch = -0.6545F * k;
-//        this.leftEar.yaw = 0.0873F * k;
-//        this.leftEar.roll = 0;
-//        this.rightEar.pitch = -0.6545F * k;
-//        this.rightEar.yaw = -0.0873F * k;
-//        this.rightEar.roll = 0;
-        // ВРЕМЕННО ИЛИ НАВСЕГДА УБРАНО
     }
 
     @Override
@@ -118,5 +94,37 @@ public class FennecEntityModel extends AnimalModel<FennecEntity> {
 
     @Override
     public void animateModel(FennecEntity entity, float limbAngle, float limbDistance, float tickDelta) {
+        var k = MathHelper.clamp(1f - entity.getHealth() / entity.getMaxHealth(), 0f, 1f);
+        // Уши (версия прижимание)
+//        this.rightEar.pitch = -0.2618F * k;
+//        this.rightEar.yaw = 1.2654F * k;
+//        this.rightEar.roll = -0.2182F * k;
+//        this.rightEar.pivotX = -1.8f - k;
+//        this.rightEar.pivotZ = -2.5F + k * 1.25f;
+//
+//        this.leftEar.pitch = -0.2618F * k;
+//        this.leftEar.yaw = -1.2654F * k;
+//        this.leftEar.roll = 0.2182F * k;
+//        this.leftEar.pivotX = 1.8f + k;
+//        this.leftEar.pivotZ = -2.5F + k * 1.25f;
+        // Уши (версия опускание)
+//        this.leftEar.pitch = -0.6545F * k;
+//        this.leftEar.yaw = 0.0873F * k;
+//        this.leftEar.roll = 0;
+//        this.rightEar.pitch = -0.6545F * k;
+//        this.rightEar.yaw = -0.0873F * k;
+//        this.rightEar.roll = 0;
+        // Бог любит троицу
+        this.rightEar.pitch = 0;
+        this.rightEar.yaw = 0;
+        this.rightEar.roll = -0.5236F * k;
+        this.rightEar.pivotY = -k * 2f;
+        this.rightEar.pivotX = -1.8f + k * 1.4f;
+
+        this.leftEar.pitch = 0;
+        this.leftEar.yaw = 0;
+        this.leftEar.roll = 0.5236F * k;
+        this.leftEar.pivotY = -k * 2f;
+        this.leftEar.pivotX = 1.8f + -k * 1.4f;
     }
 }
