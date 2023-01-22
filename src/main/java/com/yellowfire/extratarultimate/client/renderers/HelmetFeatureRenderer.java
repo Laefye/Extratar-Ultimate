3cc051f74aab624992d00fb7d815b6c157ac17d1
Laefye <rtpurtooe22@mail.ru>
2023-01-22 14:15:10 +0300

    Немного рефактора

--- src/main/java/com/yellowfire/extratarultimate/client/renderers/HelmetFeatureRenderer.java
+++ src/main/java/com/yellowfire/extratarultimate/client/renderers/HelmetFeatureRenderer.java
@@ -1,9 +1,8 @@
 package com.yellowfire.extratarultimate.client.renderers;
 
-import com.yellowfire.extratarultimate.client.models.FennecEntityModel;
-import com.yellowfire.extratarultimate.client.models.HelmetTypeAModel;
+import com.yellowfire.extratarultimate.client.models.HelmetModel;
+import com.yellowfire.extratarultimate.client.models.HelmetModels;
 import com.yellowfire.extratarultimate.items.HelmetTypeA;
-import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
@@ -15,10 +14,9 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.util.Identifier;
-import org.joml.Vector3f;
 
 public class HelmetFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
-    public HelmetTypeAModel helmetTypeA = new HelmetTypeAModel(HelmetTypeAModel.getTexturedModelData().createModel());
+    public HelmetModel helmetTypeA = new HelmetModel(HelmetModels.getTexturedModelDataHelmetTypeA());
 
     public HelmetFeatureRenderer(FeatureRendererContext<T, M> context) {
         super(context);
@@ -28,11 +26,7 @@ public class HelmetFeatureRenderer<T extends LivingEntity, M extends BipedEntity
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         var helmetStack = entity.getEquippedStack(EquipmentSlot.HEAD);
         if (!helmetStack.isEmpty() && helmetStack.getItem() instanceof HelmetTypeA helmetItem) {
-            var helmetModelPart = helmetTypeA.getHelmet();
-            helmetModelPart.copyTransform(getContextModel().head);
-            helmetModelPart.xScale *= 1.2f;
-            helmetModelPart.yScale *= 1.2f;
-            helmetModelPart.zScale *= 1.2f;
+            var helmetModelPart = helmetTypeA.getHelmet(getContextModel().head);
             var vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumers, RenderLayer.getArmorCutoutNoCull(getHelmetTexture(helmetItem.getMaterial())), false, false);
             helmetModelPart.render(matrices, vertexConsumer, light, 1);
         }
